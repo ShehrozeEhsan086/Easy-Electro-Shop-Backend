7e643d8fb89d17e6c9962947226d3d0349e0de27
ShehrozeEhsan086 <shehroze0912@gmail.com>
2023-05-22 19:19:45 +0500

    New Updates

---
+++ product-service/src/main/java/com/easyelectroshop/productservice/DTO/WebScrapperDTO/WebScrapper.java
@@ -0,0 +1,12 @@
+package com.easyelectroshop.productservice.DTO.WebScrapperDTO;
+
+import java.util.UUID;
+
+public record WebScrapper(
+    long scrapperId,
+    UUID productId,
+    String site,
+    String scrappedPrice,
+    boolean isVisible
+) {
+}
