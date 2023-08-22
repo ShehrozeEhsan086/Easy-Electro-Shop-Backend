b5a325b23362a9b49c4b6455ceadec4bb7f25686
ShehrozeEhsan086 <shehroze0912@gmail.com>
2023-08-22 14:04:52 +0500

    Name Fix for WebDriver

--- web-scrapping-service/src/main/java/com/easyelectroshop/webscrappingservice/Configuration/SeleniumConfiguration.java
+++ web-scrapping-service/src/main/java/com/easyelectroshop/webscrappingservice/Configuration/SeleniumConfiguration.java
@@ -2,8 +2,6 @@ package com.easyelectroshop.webscrappingservice.Configuration;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +12,12 @@ public class SeleniumConfiguration {
 
 //        Background Task No Chrome opening
 //        @Bean
-//        public WebDriver getChromeDriver(){
+//        public WebDriver getEdgeDriver(){
 //              return WebDriverManager.edgedriver().capabilities(new EdgeOptions().addArguments("--headless",,"--remote-allow-origins=*")).create();
 //        }
 
     @Bean
-    public WebDriver getChromeDriver(){
+    public WebDriver getEdgeDriver(){
         return WebDriverManager.edgedriver().capabilities(new EdgeOptions().addArguments("--remote-allow-origins=*")).create();
     }
 
