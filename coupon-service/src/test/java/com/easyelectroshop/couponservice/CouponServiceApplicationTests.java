0eaecb598d1662e203331172e2396d3cf24eafb3
shehroze <shehroze0912@gmail.com>
2023-10-07 22:54:54 +0500

    Coupon Service .

---
+++ coupon-service/src/test/java/com/easyelectroshop/couponservice/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyelectroshop.couponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
