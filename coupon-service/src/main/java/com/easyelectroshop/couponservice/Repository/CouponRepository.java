20a69c3560a4626f1821ae0fc2362ff7450b67d6
Shehroze Ehsan <80197565+ShehrozeEhsan086@users.noreply.github.com>
2023-10-10 15:09:56 +0500

    Get Coupon for checkout

--- coupon-service/src/main/java/com/easyelectroshop/couponservice/Repository/CouponRepository.java
+++ coupon-service/src/main/java/com/easyelectroshop/couponservice/Repository/CouponRepository.java
@@ -3,8 +3,10 @@ package com.easyelectroshop.couponservice.Repository;
 import com.easyelectroshop.couponservice.Model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface CouponRepository extends JpaRepository<Coupon,Long> {
 
-
+    Coupon getCouponByCouponCodeAndCustomerId(String couponCode, UUID customerId);
 
 }
